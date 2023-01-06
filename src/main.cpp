b09ea54958e2678d997d382c58107784c591f646
Caleb Bennett <cbennett68864@gmail.com>
2023-01-05 22:18:12 -0600

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
 // #include "M5Stack.h"
 #include "M5CoreInk.h"
+#include <iostream>
+#include <sstream> // for string streams
+#include <string>  // for string
+using namespace std;
 Ink_Sprite InkPageSprite(&M5.M5Ink);
 
-void WriteLine(String line, int num)
+void WriteLine(String line, int row)
 {
   // M5.M5Ink.clear();
   Serial.println(line);
-  InkPageSprite.drawString(35, (15 * num), line.c_str());
+  InkPageSprite.drawString(35, (15 * row), line.c_str());
+  InkPageSprite.pushSprite();
+  delay(1000);
+}
+
+void WriteColumn(String line, int row, int col, int starty)
+{
+  // M5.M5Ink.clear();
+  Serial.println(line);
+  InkPageSprite.drawString(((25 * col) + starty), (15 * row), line.c_str());
   InkPageSprite.pushSprite();
   delay(1000);
 }
@@ -20,29 +33,11 @@ void setup()
   M5.M5Ink.isInit();
   M5.M5Ink.clear();
   delay(1000);
-  // creat ink refresh Sprite
-  if (InkPageSprite.creatSprite(0, 0, 200, 200, true) != 0)
-  {
-    Serial.printf("Ink Sprite creat failed");
-  }
-  else
-  {
-    Serial.printf("creatSprite success\n");
-  }
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  WriteLine("Welcome", 1);
-  WriteLine("To", 2);
-  WriteLine("Logic", 3);
-  WriteLine("We", 4);
-  WriteLine("Build", 5);
-  WriteLine("Cool", 6);
-  WriteLine("Shit", 7);
-  WriteLine("!!!", 8);
-  delay(5000);
   M5.M5Ink.clear();
   if (InkPageSprite.creatSprite(0, 0, 200, 200, true) != 0)
   {
@@ -51,5 +46,21 @@ void loop()
   else
   {
     Serial.printf("creatSprite success\n");
+  }
+  WriteLine("Welcome", 1);
+  WriteLine("To", 2);
+  WriteLine("Logic", 3);
+  WriteLine("We", 4);
+  WriteLine("Build", 5);
+  WriteLine("Cool", 6);
+  WriteLine("Shit", 7);
+  WriteLine("!!!", 8);
+  int d = 5;
+  for (int i = 0; i < d; i++)
+  {
+    ostringstream str1;
+    str1 << (i + 1);
+    String istr = str1.str().c_str();
+    WriteColumn(istr, 9, i, 35);
   }
 }
